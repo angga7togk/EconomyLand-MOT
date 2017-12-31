4ddb51097e6a5822f5f5f30b37d8ade3748d5544
Petteri <petteri.malkavaara1@gmail.com>
2017-12-31 12:46:09 +0200

    fix

--- src/main/java/me/onebone/economyland/EconomyLand.java
+++ src/main/java/me/onebone/economyland/EconomyLand.java
@@ -816,13 +816,13 @@ public class EconomyLand extends PluginBase implements Listener{
 	
 	@EventHandler
 	public void onPlayerInteract(PlayerInteractEvent event){
-		if(event.getAction() == PlayerInteractEvent.LEFT_CLICK_AIR || event.getAction() == PlayerInteractEvent.RIGHT_CLICK_AIR) return;
+		if(event.getAction() == PlayerInteractEvent.Action.LEFT_CLICK_AIR || event.getAction() == PlayerInteractEvent.Action.RIGHT_CLICK_AIR) return;
 		
 		Player player = event.getPlayer();
 		Block block = event.getBlock();
 		Item item = event.getItem();
 		
-		if(item.canBePlaced() && !block.canBeActivated() && event.getAction() == PlayerInteractEvent.RIGHT_CLICK_BLOCK){ // placing
+		if(item.canBePlaced() && !block.canBeActivated() && event.getAction() == PlayerInteractEvent.Action.RIGHT_CLICK_BLOCK){ // placing
 			block = block.getSide(event.getFace());
 		}
 		
@@ -845,7 +845,7 @@ public class EconomyLand extends PluginBase implements Listener{
 				player.sendMessage(this.getMessage("modify-whiteland"));
 			}
 			
-			if(event.getAction() == PlayerInteractEvent.RIGHT_CLICK_BLOCK && !block.canBeActivated() && event.getItem().canBePlaced()){
+			if(event.getAction() == PlayerInteractEvent.Action.RIGHT_CLICK_BLOCK && !block.canBeActivated() && event.getItem().canBePlaced()){
 				this.placeQueue.add(player);
 			}
 		}
